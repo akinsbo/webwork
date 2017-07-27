c61392c54031c1bedbba39a6f3410b736bf61f7c
Olaolu AKinsete <akinsbo@gmail.com>
2017-07-27 10:09:04 +0100

    added jpa and sql dependencies to maven

---
+++ src/main/java/com/laveolu/webwork/WebworkApplication.java
@@ -0,0 +1,12 @@
+package com.laveolu.webwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebworkApplication.class, args);
+	}
+}
